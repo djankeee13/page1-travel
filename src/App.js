c605459667f7e5bfbec8ecf63853c1ad5da86ff8
djankeee13 <djani-b@live.com>
2022-05-10 16:21:01 +0200

    0
    0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import ImgCarousel from "./carousel/ImgCarousel";
+import Navbar from "./components/navbar/Navbar";
+import Destinations from "./destinations/Destinations";
+import Footer from "./footer/Footer";
+import Hero from "./hero/Hero";
+import Search from "./search/Search";
+import Selects from "./selects/Selects";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Hero />
+      <Destinations />
+      <Search />
+      <Selects />
+      <ImgCarousel />
+      <Footer />
     </div>
   );
 }
